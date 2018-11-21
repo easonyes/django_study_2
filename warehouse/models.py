489a6e63c32079c11b69a2f92b586039e30a4069
easonyes <2234148120@qq.com>
2018-11-21 09:34:01 +0800

    修改了模型文件

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-
-
-class Depot(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    manager = models.IntegerField() #仓库管理员的ID
 
 
 
@@ -19,18 +14,39 @@ class Employee(models.Model):
     empposit = models.CharField(max_length=200)
     empphone = models.CharField(max_length=200)
 
-    """
-       def str(self):
-           name = '1'
-           password = '1'
-           str1 = Emploee.objects.filter(empname=name, emppassword=password)
 
-           if str1:
-               order = str1.emporder
+
+
+    class Meta():
+        db_table = 'Employee'
+    """
+    def str(self):
+       name = '1'
+       password = '1'
+       str1 = Emploee.objects.filter(empname=name, emppassword=password)
+
+       if str1:
+           order = str1.emporder
 
    """
 
-class present(models.Model):
+
+class Depot(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    # manager = models.IntegerField() #仓库管理员的ID
+    manager = models.ForeignKey(
+        Employee,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
+    class Meta():
+        db_table = 'Depot'
+
+
+class Present(models.Model):
     name = models.CharField(max_length=200)
     on_date = models.DateTimeField()
     store_num = models.IntegerField()
@@ -40,3 +56,18 @@ class present(models.Model):
     off = models.IntegerField(default=0) #0代表不打折， 1代表打折
     off_cost = models.DecimalField(max_digits=3, decimal_places=2, null=True)
     url = models.CharField(max_length=200, null=True)
+    #pdepot = models.ManyToManyField(Depot, through='depot_present')
+
+    class Meta():
+        db_table = 'Present'
+
+
+class depot_present(models.Model):
+    depot = models.ForeignKey(Depot)
+    present = models.ForeignKey(Present)
+
+    add_reason = models.CharField(max_length=200, null=True)
+
+    class Meta():
+        db_table = 'depot_present'
+
