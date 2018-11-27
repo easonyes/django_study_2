179f776e9d2d29dd5c642b9a16b3158eebbc2ed0
easonyes <2234148120@qq.com>
2018-11-27 19:48:48 +0800

    基本全部完成，等待合并时需修改的地方

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('employee/<int:employee_id>/add', views.add, name='add'),
     path('employee/<int:employee_id>/gifts', views.gifts, name='gifts'),
-
+    path('employee/<int:employee_id>/modify', views.modify, name='modify'),
+    path('employee/<int:employee_id>/sell', views.sell, name='sell'),
+    path('employee/<int:employee_id>/delete', views.delete, name='delete')
 ]
--- warehouse/views.py
+++ warehouse/views.py
@@ -92,15 +92,16 @@ def login(request):
         else:
             return HttpResponse(json.dumps(context), content_type="application/json")
 
-# undone
-# not test
+
+# done
+# tested
 def logout(request):
     context={
         'IS_LOGOUT': 0
     }
     if('IS_LOGIN' in request.session and 'EMPLOYEE_ID' in request.session):
         del request.session['IS_LOGIN']
-        del request.session['EMPLOYEE']
+        del request.session['EMPLOYEE_ID']
         context['IS_LOGOUT'] = 1
         return HttpResponse(json.dumps(context), content_type="application/json")
     else:
@@ -112,7 +113,7 @@ def register(request):
     return
 
 # done
-# not test
+# tested
 """
 gifts
 返回仓库中的礼品信息，如果是仓库管理员，就返回自己仓库的信息，如果是普通员工，就返回所有礼品信息
@@ -124,7 +125,7 @@ def gifts(request, employee_id):
         'error': 0
     }
     if(not employee) or ('EMPLOYEE_ID' not in request.session) or ('IS_LOGIN' not in request.session) \
-        or (request.session['EMPLOYEE_ID' != employee_id]):
+        or (request.session['EMPLOYEE_ID'] != employee_id):
         context['error'] = 1
         return HttpResponse(json.dumps(context), content_type="application/json")
     if request.method == 'GET':
@@ -138,11 +139,8 @@ def gifts(request, employee_id):
             conx = serializers.serialize("json", presents)
             return HttpResponse(conx, content_type="application/json")
         elif employee.emporder == 2:
-            depots = Depot.objects.filter(manager=employee.id)
-            presents = []
-            for depot in depots:
-                present = Present.objects.filter(pdepot=depot.id)
-                presents.append(present)
+            depots = Depot.objects.get(manager=employee.id)
+            presents = Present.objects.filter(pdepot=depots)
             conx = serializers.serialize("json", presents)
             return HttpResponse(conx, content_type="application/json")
             # present = Present.objects.filter(pdepot=)
@@ -151,7 +149,7 @@ def gifts(request, employee_id):
             return HttpResponse(json.dumps(context), content_type="application")
     return
 
-# undone
+# done
 # tested
 """
 add
@@ -174,15 +172,9 @@ def add(request, employee_id):
         store_num = request.POST['store_num']
         status = 0
         cost = request.POST['cost']
-        hot = request.POST['hot']
-        off = request.POST['off']
-        off_cost = request.POST['off_cost']
-        if not hot:
-            hot = 0
-        if not off:
-            off = 0
-        if not off_cost:
-            off_cost = 0
+        hot = 0
+        off = 0
+        off_cost = 0
         url = request.POST['url']
         depot = Depot.objects.get(manager=employee)
 
@@ -201,19 +193,17 @@ def add(request, employee_id):
                       status=status, cost=float(cost), hot=int(hot), off=int(off),
                       off_cost=off_cost, url=url, pdepot=depot)
         obj.save()
-        # return redirect(reverse('index'))
-        return HttpResponse(json.dumps(context), content_type="application/json")
-        # return
+        present = Present.objects.filter(id=obj.id)
+        conx = serializers.serialize("json", present)
+        return HttpResponse(conx, content_type="application/json")
     return
 
 
-
-
-# undone
-# not test
+# done
+# tested
 """
 modify
-修改一个礼品的信息，仓库管理员可操作，但不可修改其状态值
+修改一个礼品的信息，仓库管理员可操作，但不可修改其状态值以及打折、热度情况
 method：PUT
 """
 def modify(request, employee_id):
@@ -226,21 +216,117 @@ def modify(request, employee_id):
         context['error'] = 1
         return HttpResponse(json.dumps(context), content_type="application/json")
     if request.method == 'PUT':
-        return
+        modify = QueryDict(request.body)
+        id = modify.get('id')
+        present = Present.objects.filter(id=id)
+        if not present:
+            context['error'] = 2
+            return HttpResponse(json.dumps(context), content_type="application/json")
+        status = present[0].status
+        name = modify.get('name')
+        introduction = modify.get('introduction')
+        on_date = modify.get('on_date')
+        store_num = modify.get('store_num')
+        cost = modify.get('cost')
+        hot = present[0].hot
+        off = present[0].off
+        off_cost = present[0].off_cost
+        url = modify.get('url')
+        pdepot = present[0].pdepot
+        present.update(name=name, introduction=introduction, status=status, on_date=on_date,
+                       store_num=store_num, cost=cost, hot=hot, off=off, off_cost=off_cost,
+                       url=url, pdepot=pdepot)
+        conx = serializers.serialize("json", present)
+        return HttpResponse(conx, content_type="application/json")
 
-# undone
-# not test
+# done
+# tested
 """
 delete
 删除一个礼品，仅仓库管理员可操作
 method：DELETE
 """
+def delete(request, employee_id):
+    employee = Employee.objects.get(pk=employee_id)
+    context = {
+        'error': 0
+    }
+    if (not employee) or ('EMPLOYEE_ID' not in request.session) or ('IS_LOGIN' not in request.session) \
+            or (request.session['EMPLOYEE_ID'] != employee_id):
+        context['error'] = 1
+        return HttpResponse(json.dumps(context), content_type="application/json")
+    if request.method == 'DELETE':
+        delete = QueryDict(request.body)
+        key = delete.get('id')
+        present = Present.objects.filter(id=key)
+        if not present:
+            context['error'] = 2
+            return HttpResponse(json.dumps(context), content_type="application/json")
+        conx = serializers.serialize("json", present)
+        present.delete()
+        return HttpResponse(conx, content_type="application/json")
 
-# undone
-# not test
+# done
+# tested
 """
 sell
-修改一个礼品的状态值，即上架或下架该礼品，仅普通员工可操作
+修改一个礼品的状态值，即上架或下架该礼品，以及修改其打折、热度情况，仅普通员工可操作
 method：PUT
 """
+def sell(request, employee_id):
+    employee = Employee.objects.get(pk=employee_id)
+    context = {
+        'error': 0
+    }
+    if (not employee) or ('EMPLOYEE_ID' not in request.session) or ('IS_LOGIN' not in request.session) \
+            or (request.session['EMPLOYEE_ID'] != employee_id):
+        context['error'] = 1
+        return HttpResponse(json.dumps(context), content_type="application/json")
+    if request.method == 'PUT':
+        sell = QueryDict(request.body)
+        key = sell.get('id')
+        present = Present.objects.filter(id=key)
+        if not present:
+            context['error'] = 2
+            return HttpResponse(json.dumps(context), content_type="application/json")
+        name = present[0].name
+        introduction = present[0].introduction
+        on_date = present[0].on_date
+        store_num = present[0].store_num
+        status = sell.get('status')
+        cost = present[0].cost
+        hot = sell.get('hot')
+        off = sell.get('off')
+        off_cost = sell.get('off_cost')
+        url = present[0].url
+        pdepot = present[0].pdepot
+        present.update(name=name, introduction=introduction, status=status, on_date=on_date,
+                       store_num=store_num, cost=cost, hot=hot, off=off, off_cost=off_cost,
+                       url=url, pdepot=pdepot)
+        conx = serializers.serialize("json", present)
+        return HttpResponse(conx, content_type="application/json")
+    if request.method == 'GET':
+        depots = QueryDict(request.body)
+        key1 = depots.get('depot_id')
+        key2 = depots.get('present_status')
+        if key1:
+            depot = Depot.objects.get(id=key1)
+            presents = Present.objects.filter(pdepot=depot)
+            conx = serializers.serialize("json", presents)
+            return HttpResponse(conx, content_type="application/json")
+        if key2:
+            if key2 == '1':
+                presents = Present.objects.filter(status=key2)
+                conx = serializers.serialize("json", presents)
+                return HttpResponse(conx, content_type="application/json")
+            elif key2 == '0':
+                presents = Present.objects.filter(status=key2)
+                conx = serializers.serialize("json", presents)
+                return HttpResponse(conx, content_type="application/json")
+            elif key2 == '2':
+                presents = Present.objects.filter(status=key2)
+                conx = serializers.serialize("json", presents)
+                return HttpResponse(conx, content_type="application/json")
+
+
 
